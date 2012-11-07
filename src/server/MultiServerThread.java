17d1ca042ea1acbcd916cfde987a8c20317a712a
Kristoffer <kristoffersilverhav@gmail.com>
2012-11-07 14:25:09 +0100

    felsökning

--- src/server/MultiServerThread.java
+++ src/server/MultiServerThread.java
@@ -138,7 +138,7 @@ public class MultiServerThread extends Thread {
 			Contact cont = (Contact) m;
 			System.out.println("cont name: " + cont.getContactName() + "    msg reciever: " + msg.getReciever() + "     cont inet: " + cont.getInetAddress());
 			if (cont.getContactName().equals(msg.getReciever().toString())
-					&& (hashMap.keySet().contains(socket.getInetAddress().toString()))) {
+					&& (hashMap.keySet().contains("/"+cont.getInetAddress().toString()))) {
 				System.out.println("Nu skickar den vidare skiten");
 				send(message, hashMap.get(cont.getInetAddress().toString()));
 			}
