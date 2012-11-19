fe0776b1a7692d9bd54031b8ee1978dce3959ee8
Kristoffer <kristoffersilverhav@gmail.com>
2012-11-19 12:54:02 +0100

    felsök

--- src/server/MultiServerThread.java
+++ src/server/MultiServerThread.java
@@ -138,6 +138,7 @@ public class MultiServerThread extends Thread {
 			if (server.send(message, msg.getReciever().toString())) {
 				msg.setSent(true);
 			} else {
+				System.out.println("här ska den inte hamna");
 				server.addUnsentItem(msg);
 			}
 			db.addToDB(msg);
