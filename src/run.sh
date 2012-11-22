b6c6127ce6be9ed502d02d91472a9f90532a8fd8
kettel <wiktor@kettel.se>
2012-11-22 18:15:44 +0100

    La in i run ett meddelande om att kompileringen är färdig.

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 v=$(echo ../lib/*.jar | tr ' ' ':../')
 javac -cp .:../$v  database/*.java model/*.java server/*.java jetty/*.java
+echo "Kompilerat färdigt. Nu ska servern startas!"
 java -cp .:../$v  server.Server
