26052e4803b5c407474065832619ff06bd6c705e
Kristoffer <kristoffersilverhav@gmail.com>
2012-11-23 16:34:00 +0100

    syso

--- src/server/CreateContactCommand.java
+++ src/server/CreateContactCommand.java
@@ -95,7 +95,7 @@ public class CreateContactCommand implements CommandInterface {
 	
 	private String readPw(){
 		char[] pw;
-		if((console = System.console()) != null && (pw = console.readPassword("[%s]", "Password: ")) != null){
+		if((console = System.console()) != null && (pw = console.readPassword("Password: ")) != null){
 			java.util.Arrays.fill(pw, ' ');
 			return pw.toString();
 		}
