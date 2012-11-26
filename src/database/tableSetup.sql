0aa505563bf19ff4462786b4caf214cc743bc8bd
Wiktor Kettel <wiktor@kettel.se>
2012-11-26 21:54:12 +0100

    hrm.. rättigheter.

--- src/database/tableSetup.sql
+++ src/database/tableSetup.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE TDDD36;
 USE TDDD36;
 CREATE USER 'serverUser'@'localhost' IDENTIFIED BY 'handdukMandel';
-GRANT ALL PRIVILEGES ON TDDD36 TO serverUser;
+GRANT ALL PRIVILEGES ON TDDD36 TO serverUser WITH GRANT OPTION;
 # Logga ut
 exit;
 # Logga in som serverUser
