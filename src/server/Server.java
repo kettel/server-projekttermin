6effb502035e740c18741699a3d023406b117f87
Kristoffer <kristoffersilverhav@gmail.com>
2012-11-29 16:48:43 +0100

    testar resend vid pull

--- src/server/Server.java
+++ src/server/Server.java
@@ -203,7 +203,7 @@ public class Server {
 						QueueItem qItem = (QueueItem) m;
 						pr.println(qItem.getJSON());
 						db.deleteFromDB(qItem);
-						System.out.println("Sending " + qItem.getDatabaseRepresentation() + " from queue to " + receiver);
+						System.out.println("Sending " + qItem.getJSON() + " from queue to " + receiver.getContactName());
 					}
 				}
 			} catch (Exception e) {
