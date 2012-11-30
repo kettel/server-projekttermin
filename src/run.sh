d2224c51af38d648b5a2bb7d8c608dede02adcd4
giraff hals <giraff@giraff.(none)>
2012-11-30 13:33:26 +0100

    Modifierat bash så att man inte behöver säga vilken server

--- src/run.sh
+++ src/run.sh
@@ -4,18 +4,18 @@ server="192.168.1.128"
 
 if [ $ip=$server ]
 	then 
-		port=17234
-		jetty=16783
-		v=$(echo ../lib/*.jar | tr ' ' ':../')
-		javac -cp .:../$v  database/*.java model/*.java server/*.java jetty/*.java
-		echo "Kompilerat färdigt. Nu ska servern startas!"
-		java -cp .:../$v server.Server $port $jetty
-else
 	port=18234
 	jetty=17783
 	v=$(echo ../lib/*.jar | tr ' ' ':../')
 	javac -cp .:../$v  database/*.java model/*.java server/*.java jetty/*.java
 	echo "Kompilerat färdigt. Nu ska servern startas!"
 	java -cp .:../$v -Djava.awt.headless=true server.Server $port $jetty
+else
+	port=17234
+	jetty=16783
+	v=$(echo ../lib/*.jar | tr ' ' ':../')
+	javac -cp .:../$v  database/*.java model/*.java server/*.java jetty/*.java
+	echo "Kompilerat färdigt. Nu ska servern startas!"
+	java -cp .:../$v server.Server $port $jetty
 fi
 
