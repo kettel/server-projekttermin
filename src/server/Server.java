69b4eca2af9ccfe868440e9e846a9933ccf5e859
Kristoffer <kristoffersilverhav@gmail.com>
2012-12-03 11:41:43 +0100

    syso

--- src/server/Server.java
+++ src/server/Server.java
@@ -210,7 +210,7 @@ public class Server {
 			gcmMap.put(name, gcmId);
 		} else {
 			System.out.println("remove!!!");
-			removeClient(name);
+			removeGcmClient(name);
 			System.out.println("Adding " + name + " to gcmMap");
 			gcmMap.put(name, gcmId);
 		}
