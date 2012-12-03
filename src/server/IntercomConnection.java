17e552a2e95e49f0c3aabeaa5272af1696bdb104
Eric Karlsson <erika440@student.liu.se>
2012-12-03 13:17:49 +0100

    ready for some testing

--- src/server/IntercomConnection.java
+++ src/server/IntercomConnection.java
@@ -7,9 +7,11 @@ import intercomModels.MissionIntergroup;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Type;
 import java.security.KeyStore;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.Queue;
 import javax.net.ssl.HandshakeCompletedEvent;
@@ -24,6 +26,7 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import model.Assignment;
+import model.AssignmentStatus;
 
 public class IntercomConnection  extends Thread implements HandshakeCompletedListener{
 	private static String serverIP = "ipgoeshere";
@@ -41,8 +44,12 @@ public class IntercomConnection  extends Thread implements HandshakeCompletedLis
 	private TrustManagerFactory trustManagerFactory = null;
 	private SSLSocketFactory sslSocketFactory = null;
 	private Queue <String> intercomQueue = new LinkedList<String>();
+	private WgspointConverter WgsC = new WgspointConverter();
 	private Gson gson = new Gson();
-	public IntercomConnection(){
+	private Server server = null;
+	
+	public IntercomConnection(Server server){
+		this.server = server;
 			try {
 				keystore = KeyStore.getInstance("JKS");
 				keystore.load(new FileInputStream(new File(getClass().getClassLoader().getResource("server/masterserver.jks").getPath())),password);
@@ -61,11 +68,12 @@ public class IntercomConnection  extends Thread implements HandshakeCompletedLis
 	
 	public synchronized void closeConncetion(){
 		this.stayConnected = false;
+		setConnected(false);
 	}
 	
 	private synchronized void stayConnected(){
 		this.stayConnected = true;
-		setConnected(false);
+		setConnected(true);
 	}
 	
 	private synchronized boolean isStayConnected(){
@@ -81,10 +89,16 @@ public class IntercomConnection  extends Thread implements HandshakeCompletedLis
 	}
 	
 	public synchronized void addIntercomAssignment(Assignment assigmentToSend){
-		
-//		new MissionIntergroup(new MissionID(faction, id), new GPSCoordinate(, latitude), title, description, creationTime)
+		String region = assigmentToSend.getRegion();
+		double lon = WgsC.WgsStringToDoubels(region)[0];
+		double lat = WgsC.WgsStringToDoubels(region)[1];
+		MissionIntergroup conveted = new MissionIntergroup(new MissionID(faction, id), new GPSCoordinate(lon, lat), assigmentToSend.getName(), assigmentToSend.getAssignmentDescription(), new Date(assigmentToSend.getTimeStamp()));
+		String outgoing = gson.toJson(conveted);
+		intercomQueue.add(outgoing);
 	}
-	
+	private synchronized Queue <String> getQueue(){
+		return intercomQueue;
+	}
 	
 	public void run(){
 		while(isStayConnected()){
@@ -93,7 +107,7 @@ public class IntercomConnection  extends Thread implements HandshakeCompletedLis
 				sslSocket = (SSLSocket) sslSocketFactory.createSocket(serverIP,serverPort);
 				sslSocket.addHandshakeCompletedListener(this);
 				sslSocket.startHandshake();
-				while(!isConnected() && isStayConnected()){
+				if(!isConnected() && isStayConnected()){
 					this.wait(500);	
 				}
 			} catch (Exception e) {
@@ -101,11 +115,57 @@ public class IntercomConnection  extends Thread implements HandshakeCompletedLis
 				setConnected(false);
 			}
 			while(isConnected()){
-				
+				// get stuff thread
+				new Thread(){
+					public void run() {
+						while(isConnected()){
+							try {
+								String incomeing = input.readLine();
+								if(incomeing != "&"){
+									if(incomeing.contains("\"identifier\":\"@Missonintergroup@\"")){
+										System.out.println("incoming misson fron itercom server");
+										MissionIntergroup intercom = gson.fromJson(incomeing, MissionIntergroup.class);
+										double[] latAndLon = {intercom.getLocation().getLatitude(),intercom.getLocation().getLongitude()};
+										String region = WgsC.DoubelToGsonWgsString(latAndLon);
+										Assignment misson = new Assignment(intercom.getTitle(),region,"intercom", false, intercom.getDescription(), "", AssignmentStatus.STARTED, "", "");
+										server.sendToAll(gson.toJson(misson));
+									}else if(incomeing.contains("\"identifier\":\"@MissonUpdateInter@\"")){
+										System.out.println("uppdate, nothing done");
+									}
+								}
+							} catch (Exception e) {
+								System.out.println("Crash in intercom input thread, due to " + e.toString() + System.getProperty("line.separator") + "Dissconnecting from intercom server");
+								setConnected(false);
+							}
+						}
+					};
+				}.start();
+				//send stuff
+				new Thread(){
+					public void run(){
+						while(isConnected()){
+						try {
+							 Queue <String> q = getQueue();
+							if(!q.isEmpty()){
+								output.println(q.peek());
+								if(output.checkError()){
+									setConnected(false);
+									System.out.println("Dissconnecting from intercom server");
+								}else{
+									q.poll();
+								}
+							}
+							this.wait(50);
+						} catch (Exception e) {
+						System.out.println("Crash in output thread due to: " + e.toString() + System.getProperty("line.separator") + "Dissconnecting from intercom server");
+						}
+						}
+					}
+				}.start();
 			}
-			
 		}
 	}
+	
 	@Override
 	public void handshakeCompleted(HandshakeCompletedEvent arg0) {
 		System.out.println("SSL handshake with intercom server compleated");
