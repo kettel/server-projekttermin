ad9ab61b9b323fc62091e22cc6771bd9bca6c0fd
Kristoffer <kristoffersilverhav@gmail.com>
2012-12-04 09:25:33 +0100

    testar lägga alla GCMIDn from db i en lista för att känna igen registrerade IDn vid uppstart av server

--- src/gcm/Datastore.java
+++ src/gcm/Datastore.java
@@ -19,6 +19,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import model.Contact;
+import model.ModelInterface;
+import database.Database;
+
 /**
  * Simple implementation of a data store using standard Java collections.
  * <p>
@@ -27,52 +31,59 @@ import java.util.logging.Logger;
  */
 public final class Datastore {
 
-  private static final List<String> regIds = new ArrayList<String>();
-  private static final Logger logger =
-      Logger.getLogger(Datastore.class.getName());
+	private static final List<String> regIds = new ArrayList<String>();
+	private static final Logger logger = Logger.getLogger(Datastore.class
+			.getName());
+	private List<ModelInterface> contactList = new ArrayList<ModelInterface>();
+	private Database db = new Database();
 
-  private Datastore() {
-    throw new UnsupportedOperationException();
-  }
+	private Datastore() {
+		contactList = db.getAllFromDB(new Contact());
+		for (ModelInterface m : contactList) {
+			Contact cont = (Contact) m;
+			regIds.add(cont.getGcmId());
+		}
+		throw new UnsupportedOperationException();
+	}
 
-  /**
-   * Registers a device.
-   */
-  public static void register(String regId) {
-    logger.info("Registering " + regId);
-    synchronized (regIds) {
-      regIds.add(regId);
-    }
-  }
+	/**
+	 * Registers a device.
+	 */
+	public static void register(String regId) {
+		logger.info("Registering " + regId);
+		synchronized (regIds) {
+			regIds.add(regId);
+		}
+	}
 
-  /**
-   * Unregisters a device.
-   */
-  public static void unregister(String regId) {
-    logger.info("Unregistering " + regId);
-    synchronized (regIds) {
-      regIds.remove(regId);
-    }
-  }
+	/**
+	 * Unregisters a device.
+	 */
+	public static void unregister(String regId) {
+		logger.info("Unregistering " + regId);
+		synchronized (regIds) {
+			regIds.remove(regId);
+		}
+	}
 
-  /**
-   * Updates the registration id of a device.
-   */
-  public static void updateRegistration(String oldId, String newId) {
-    logger.info("Updating " + oldId + " to " + newId);
-    synchronized (regIds) {
-      regIds.remove(oldId);
-      regIds.add(newId);
-    }
-  }
+	/**
+	 * Updates the registration id of a device.
+	 */
+	public static void updateRegistration(String oldId, String newId) {
+		logger.info("Updating " + oldId + " to " + newId);
+		synchronized (regIds) {
+			regIds.remove(oldId);
+			regIds.add(newId);
+		}
+	}
 
-  /**
-   * Gets all registered devices.
-   */
-  public static List<String> getDevices() {
-    synchronized (regIds) {
-      return new ArrayList<String>(regIds);
-    }
-  }
+	/**
+	 * Gets all registered devices.
+	 */
+	public static List<String> getDevices() {
+		synchronized (regIds) {
+			return new ArrayList<String>(regIds);
+		}
+	}
 
 }
