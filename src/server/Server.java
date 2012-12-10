ba8a31c3d024603725f45f3efa6b98a79818dac3
Kristoffer <kristoffersilverhav@gmail.com>
2012-12-10 13:58:16 +0100

    notifierar en klienter vid uppdatering av ass och cont

--- src/server/Server.java
+++ src/server/Server.java
@@ -255,13 +255,6 @@ public class Server {
 					QueueItem qItem = new QueueItem(cont.getId(),
 							stringToBeSent);
 					db.addToDB(qItem);
-//					try {
-//						new SendAll().sendAll();
-//					} catch (IOException e) {
-//						// TODO Auto-generated catch block
-//						System.out.println("fel med GCM doPost");
-//						e.printStackTrace();
-//					}
 				}
 			}
 		}
